d1d99bb664cb4f938fd9031594aa20298637ba41
payoux <claire.payoux@epfl.ch>
2020-11-24 09:33:46 +0100

    gestion erreurs dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,13 @@
 Random* _RNG = new Random();
 
 int main(int argc, char** argv){
-    Simulation sim(argc, argv);
-    sim.run();
-    if (_RNG) delete _RNG;
+    try {
+        Simulation sim(argc, argv);
+        sim.run();
+        if (_RNG) delete _RNG;
+    } catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
     return 0;
 }
