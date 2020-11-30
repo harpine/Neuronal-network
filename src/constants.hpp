31e58d8d6818d2292090685e628e6862543b6c63
payoux <claire.payoux@epfl.ch>
2020-11-30 14:05:20 +0100

    xor removed

--- src/constants.hpp
+++ src/constants.hpp
@@ -12,7 +12,7 @@
 #define _DELTA_T_ .5
 #define _NB_ 10000
 #define _PERC_ .5
-#define _REP_ "FS:0.5"
+#define _TYPE_ "FS:0.5"
 #define _LAMB_ 10
 #define _INT_ 8 // TODO: decide what will be the default parameter
 #define _MOD_ 'b'
@@ -67,7 +67,7 @@
 #define _TIME_TEXT_ "Simulation end time"
 #define _NEURON_NUMBER_ "Number of neuron in the simulation"
 #define _PERCENT_ACTIVE_ "Percentage of exciting neurons.\nNeeds to be between 0 and 1"
-#define _REP_TEXT_ "Proportion of differents neurons.\nTypes: IB,CH,LTS,FS,TC,RZ the remaining propotion will be RS.\n Sum (including RS) is 1 \n Format is TYPE1:proportion,TYPE2:proportion,..."
+#define _TYPE_TEXT_ "Proportion of differents neurons.\nTypes: IB,CH,LTS,FS,TC,RZ the remaining propotion will be RS.\n Sum (including RS) is 1 \n Format is TYPE1:proportion,TYPE2:proportion,..."
 #define _LAMBDA_ "Mean connectivity between the neurons"
 #define _INTENSITY_ "Mean intensity of a connection"
 #define _PRGRM_TEXT_ "Neuron simulation"
