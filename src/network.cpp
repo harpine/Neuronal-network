66eacfa3d003ef7af03099b441a63ecff0c3b8b6
payoux <claire.payoux@epfl.ch>
2020-12-05 15:16:19 +0100

    netword format edition and doc modifications

--- src/network.cpp
+++ src/network.cpp
@@ -9,14 +9,12 @@ Network::Network(char model, int nb, double p_E, double intensity, double lambda
 {
     Neuron* neuron;
     int excit(p_E * nb);
-    for (int i(0); i < nb - excit; ++i)
-    {
+    for (int i(0); i < nb - excit; ++i) {
         neuron = new InhibitoryNeuron(delta);
         _network.push_back(neuron);
         _neuronsforoutputs[0] = neuron;
     }
-    for (int i(0); i < excit; ++i)
-    {
+    for (int i(0); i < excit; ++i) {
         neuron = new ExcitatoryNeuron(delta);
         _network.push_back(neuron);
         _neuronsforoutputs[6] = neuron;
@@ -30,29 +28,25 @@ Network::Network(char model, int nb, double p_FS, double p_IB, double p_RZ, doub
     Neuron* neuron;
     std::vector<std::string> type = {"FS", "LTS", "IB", "RZ", "TC", "CH", "RS"};
     int fs(nb*p_FS);
-    for (int i(0); i < fs; i++)
-    {
+    for (int i(0); i < fs; i++) {
         neuron = new InhibitoryNeuron(delta, type[0]);
         _network.push_back(neuron);
         _neuronsforoutputs[0] = neuron;
     }
     int lts(nb*p_LTS);
-    for (int i(0); i < lts; i++)
-    {
+    for (int i(0); i < lts; i++) {
         neuron = new InhibitoryNeuron(delta, type[1]);
         _network.push_back(neuron);
         _neuronsforoutputs[1] = neuron;
     }
     int ib(nb*p_IB);
-    for (int i(0); i < ib; i++)
-    {
+    for (int i(0); i < ib; i++) {
         neuron = new ExcitatoryNeuron(delta, type[2]);
         _network.push_back(neuron);
         _neuronsforoutputs[2] = neuron;
     }
     int rz(nb*p_RZ);
-    for (int i(0); i < rz; i++)
-    {
+    for (int i(0); i < rz; i++) {
         neuron = new ExcitatoryNeuron(delta, type[3]);
         _network.push_back(neuron);
         _neuronsforoutputs[3] = neuron;
@@ -70,8 +64,7 @@ Network::Network(char model, int nb, double p_FS, double p_IB, double p_RZ, doub
         _neuronsforoutputs[5] = neuron;
     }
 
-    for (int i(0); i < (nb - fs - lts - ib - rz - tc - ch); i++)
-    {
+    for (int i(0); i < (nb - fs - lts - ib - rz - tc - ch); i++) {
         neuron = new ExcitatoryNeuron(delta, type[6]);
         _network.push_back(neuron);
         _neuronsforoutputs[6] = neuron;
@@ -82,73 +75,58 @@ Network::Network(char model, int nb, double p_FS, double p_IB, double p_RZ, doub
 
 Network::~Network()
 {
-    for (auto& neuron: _network)
-    {
+    for (auto& neuron: _network) {
         delete neuron;
         neuron = nullptr;
     }
 }
-void Network::makeConnections(double lambda)
-{
+void Network::makeConnections(double lambda) {
     std::map<Neuron*, double> connections;
     bool avoidProblem(false);
-    for (size_t i(0); i<_network.size(); i++)
-    {
+    for (size_t i(0); i<_network.size(); i++) {
         connections.clear(); //avoid to recreate a new temporary map for each neuron
         int nbConnections;
-        if (_model == 'c')
-        {
+        if (_model == 'c') {
             nbConnections = int(lambda);
         }
-        else if(_model == 'o')
-        {
+        else if(_model == 'o') {
             nbConnections = _RNG->poisson(_RNG->exponential(1/lambda));
         }
-        else //if another letter is used, we use the basic model
-        {
+        else {
             nbConnections = _RNG->poisson(lambda);
         }
-        for (int j(0); j < std::min(nbConnections, int(_network.size())-1) and _connections.size() < _network.size(); j++)
-        // we have to take the minimum of both, because the distribution result can be higher than lambda and make an error occuring.
-        {
-            //pick a random neuron and connect it to the actual neurons. -> using which distribution ??
-            size_t k(_RNG->uniform_int(0, _network.size()-1)); //because k is the index, so we have to subtract 1 to the total size
-            while (connections.find(_network[k]) != connections.end() or k == i) //avoid to have 2 times the same neuron
-            {
-                k+=1; //isn't really random, but can avoid to repeat thousands of time the uniform distribution and to fall in an infinite loop
-                if (k > _network.size()-1)
-                {
-                        if (avoidProblem)
-                        {
-                           throw std::domain_error ("this neuron is already connected to all neurons of the network");
-                        }
-                    k= 0;
-                    avoidProblem = true;
+        for (int j(0); j < std::min(nbConnections, int(_network.size())-1) and _connections.size() < _network.size(); j++) {
+        //we have to take the minimum of both, because the distribution result can be higher than lambda and make an error occuri
+            size_t k(_RNG->uniform_int(0, (_network.size() - 1)));//pick a random neuron and connect it to the actual neurons
+            //avoid to check the same neurons several times
+            while (connections.find(_network[k]) != connections.end() or k == i) {
+                k+=1; //avoid an infinite loop
+                if (k > (_network.size() - 1)){
+                    if (avoidProblem) {
+                        throw std::domain_error ("this neuron is already connected to all neurons of the network");
+                    }
+                k= 0;
+                avoidProblem = true;
                 }
             }
             avoidProblem = false;
-            connections.insert(std::make_pair(_network[k], _network[k]->factor() * _RNG->uniform_double(0, 2*_intensity)));
+            connections.insert(std::make_pair(_network[k], _network[k]->factor()*_RNG->uniform_double(0, 2*_intensity)));
         }
         _connections.push_back(connections);
     }
 }
 
-void Network::update()
-{
-    for (size_t i(0); i <_network.size(); i++)
-    {
+void Network::update() {
+    for (size_t i(0); i <_network.size(); i++) {
         synapticCurrent(i);
         _network[i]->update();
     }
 }
 
-void Network::synapticCurrent(int index)
-{
+void Network::synapticCurrent(int index) {
     double input(0);
-    for (auto& pair: _connections[index])
-    {
-        if (pair.first->isFiring())
-        {
+    for (auto& pair: _connections[index]) {
+        if (pair.first->isFiring()) {
             input += pair.second;
         }
     }
@@ -156,35 +134,28 @@ void Network::synapticCurrent(int index)
 
 }
 
-std::vector<bool> Network::getCurrentstatus() const
-{
+std::vector<bool> Network::getCurrentstatus() const {
     std::vector<bool> status;
-    for (size_t i(0); i < _network.size(); i++)
-    {
+    for (size_t i(0); i < _network.size(); i++) {
         status.push_back(_network[i]->isFiring());
     }
     return status;
 }
 
-std::vector<Neuron*> Network::getNet() const
-{
+std::vector<Neuron*> Network::getNet() const {
     return _network ;
 }
-std::vector<std::map<Neuron*, double>> Network::getCon() const
-{
+std::vector<std::map<Neuron*, double>> Network::getCon() const {
     return _connections;
 }
 
-std::array<Neuron*,7> Network::getNeuronsOutput() const
-{
+std::array<Neuron*,7> Network::getNeuronsOutput() const {
     return _neuronsforoutputs;
 }
 
-double Network::getValence(int index) const
-{
+double Network::getValence(int index) const {
     double input(0);
-    for (auto& pair: _connections[index])
-    {
+    for (auto& pair: _connections[index]) {
             input += pair.second;
     }
     return input;
--- src/network.hpp
+++ src/network.hpp
@@ -8,107 +8,113 @@
 
 
 /**
- * @brief Class that handles the network of neurons.
+ * @brief Class that handles the Network of neurons.
  * 
- * Network class implement the notion of network, connecting the neurons together and handling the interactions between them.
- * It is defined by a set of neurons and associated connections and intensities, the mean intensity of the connections, a model for the connection
+ *  The Network class implements the notion of network, connecting the neurons together and handling the interactions between them.
+ * It is defined by a set of neurons and associated connections with their intensities, the mean intensity of the connections and a model for the connections.
 */
 
 class Network {
 
 public:
 
-/*! @brief Basic constructor.
-    Initialize the network by adding the inhibitory & excitatory neurons, given a proportion of excitatory neurons.
-    Initialize connection given a model and a mean intensity for the connections.
-    @param model the model of connection
-    @param nb the number of neurons wanted
-    @param p_E the proportion of excitatory neurons
-    @param intensity the mean intensity of connection
-    @param lambda the mean connectivity between neurons
-    @param delta the variability around 1 of distribution of noise
+  /*! @brief Basic constructor.
+      Initializes the network by adding the inhibitory & excitatory neurons, given a proportion of excitatory neurons.
+      Initializes connections given a model and a mean intensity for the connections.
+      @param model the model of connection
+      @param nb the number of neurons wanted
+      @param p_E the proportion of excitatory neurons
+      @param intensity the mean intensity of connection
+      @param lambda the mean connectivity between neurons
+      @param delta the variability around 1 of distribution of noise
     */
-Network(char model, int nb, double p_E, double intensity, double lambda, double delta);
+  Network(char model, int nb, double p_E, double intensity, double lambda, double delta);
 
-/*! @brief Constructor with extended neurons types.
-    Initialize the network by adding the neurons, given the different types proportions.
-    Initialize connection given a model and a mean intensity for the connections.
-    Each type has to be explicitly given, except the type RS, which will be the rest of the neurons.
-    @param model the model of connection
-    @param nb the number of neurons wanted
-    @param p_FS the proportion of neurons of type FS
-    @param p_IB the proportion of neurons of type IB
-    @param p_RZ the proportion of neurons of type RZ
-    @param p_LTS the proportion of neurons of type LTS
-    @param p_TC the proportion of neurons of type TC
-    @param p_CH the proportion of neurons of type CH
-    @param intensity the mean intensity of connection
-    @param lambda the mean connectivity between neurons
-    @param delta the variability around 1 of distribution of noise
+  /*! @brief Constructor with extended neurons types.
+      Initializes the network by adding the neurons, given the different types proportions.
+      Initializes connection given a model and a mean intensity for the connections.
+      Each type has to be explicitly given, except the RS one, which will be the rest of the neurons.
+      @param model the model of connection
+      @param nb the number of neurons wanted
+      @param p_FS the proportion of neurons of type FS
+      @param p_IB the proportion of neurons of type IB
+      @param p_RZ the proportion of neurons of type RZ
+      @param p_LTS the proportion of neurons of type LTS
+      @param p_TC the proportion of neurons of type TC
+      @param p_CH the proportion of neurons of type CH
+      @param intensity the mean intensity of connection
+      @param lambda the mean connectivity between neurons
+      @param delta the variability around 1 for the distribution of the noise
     */
-Network(char model, int nb, double p_FS, double p_IB, double p_RZ, double p_LTS, double p_TC, double p_CH, double intensity, double lambda, double delta);
+  Network(char model, int nb, double p_FS, double p_IB, double p_RZ, double p_LTS, double p_TC, double p_CH, double intensity, double lambda, double delta);
 
-/*! @brief Destructor.
-  Destroy all neurons in the set
+  /*! @brief Destroys all neurons in the set*/
+  ~Network();
+
+  /*! @brief Initializes the connections.
+  * Determines the number of connections for each neuron, given a mean number of connections and depending on the model chosen.
+  * If the model is <b>basic</b> ("b"), the number of connection for each neuron is around the mean and only few of them are making extreme numbers of connections.
+  * If the model is <b>constant</b> ("c"), the number of connection for each neuron is the same and equals lambda.
+  * If the model is <b>overdisplayed</b> ("o"), the number of connection for each neuron is overdisplayed, meaning that there is less and less neurons making a bigger number of connection.
+  * Connects randomly the neurons 
+  * @param lambda , the mean parameter used to compute how many connection a number will make.
   */
-~Network();
+  void makeConnections(double lambda);
 
-/*! @brief Initialize connections.
- * Determine the number of connections for each neuron, given a mean number of connections and depending on the model chosen. 
- * Connect the neurons to the other that will affect their current.
- * @param lambda mean of distribution to know how many connections the neuron will make
-*/
-void makeConnections(double lambda);
+  /*! @brief Updates the neurons*/
+  void update();
 
-/*! @brief update the neurons.
- * Allows to update all the neurons.
- */
- void update();
-
-/*! @brief Calculate synaptic current received by the neurons.
- * Calculate the synaptic current that the neuron receive and set the current received in the neuron attribute.
- * @param index The index of the neuron on which we want to caculate the total current.
- */
- void synapticCurrent(int index);
-
-/*! @brief return a list of 0 & 1, imaging if a vector is firing or not.
- * 0 = hasn't fired this step of time, 1 = fired this step of time
- * @return the list of neurons saying if each neuron is firing (1) or not (0)
- */
- std::vector<bool> getCurrentstatus() const;
-
- /*! @brief get the list of the neurons in the network
-  * @return the list of the neurons composing the networks
+  /*! @brief Calculates the synaptic current received by the neurons, and sets the new current.
+  * @param index The index of the neuron for which we want to caculate the total current.
   */
- std::vector<Neuron*> getNet() const;
+  void synapticCurrent(int index);
 
-/*! @brief get the list of the connections of neurons in the network
-* @return the vector of connections of the network
-*/
-std::vector<std::map<Neuron*, double>> getCon() const;
+  /*! @brief return a list of 0 & 1, showing if a neuron is firing or not.
+  * 0 = did not fire this step of time, 1 = fired this step of time
+  * @return the list of neurons saying if each neuron is firing (1) or not (0)
+  */
+  std::vector<bool> getCurrentstatus() const;
 
-/*! @brief Get one neuron of each type and put it in a list  
- * @note The order of the type of neurons is "FS", "LTS", "IB", "RZ", "TC", "CH", "RS"
- * @note If one of the type is not present in the network, the array contains a nullptr at it position.
- * @return An array containing each type of neuron
- */
-std::array<Neuron*,7> getNeuronsOutput() const;
+  /*! @brief Getter for the set of the neurons in the network
+    * @return the list of neurons composing the network
+    */
+  std::vector<Neuron*> getNet() const;
 
-/*! @brief get Valence of neuron given in parameters
- * @return the sum of pondered intensity of current
- */
- double getValence(int index) const;
+  /*! @brief Getter for the connections of neurons in the network
+   *  @return the vector of connections of the network
+   */
+  std::vector<std::map<Neuron*, double>> getCon() const;
 
+  /*! @brief Getter for one neuron of each type, putting it in a list  
+   *  @note The order of the type of neurons is "FS", "LTS", "IB", "RZ", "TC", "CH", "RS"
+   *  @note If one of the type is not present in the network, the array contains a nullptr at it position.
+   *  @return An array containing each type of neuron
+   */
+  std::array<Neuron*,7> getNeuronsOutput() const;
+
+  /*! @brief Getter for the Valence of a neuron
+      @param index to access this specific neuron within the network 
+      @return The sum of pondered intensity of current
+   */
+  double getValence(int index) const;
 
 private:
-std::vector<Neuron*> _network;
-/*! Collection of connections to the neurons of corresponding index.
- * The second parameter of the map is the intensity of the connection.
-*/
-std::vector<std::map<Neuron*, double>> _connections;
-double _intensity;
-char _model;
-std::array<Neuron*,7> _neuronsforoutputs; ///order is FS, LTS, IB, RZ, TC, CH, RS
+  ///Collection of all neurons of the network
+  std::vector<Neuron*> _network;
+
+  ///Collection of connections to the neurons of corresponding index.
+  ///The second parameter of the map is the intensity of the connection.
+  std::vector<std::map<Neuron*, double>> _connections;
+
+  ///The mean intensity for the connections
+  double _intensity;
+
+  ///The model of the simulation
+  char _model;
+
+  ///One neuron of each type present in the simulation to compute the output graphs
+  ///The order is FS, LTS, IB, RZ, TC, CH, RS
+  std::array<Neuron*,7> _neuronsforoutputs; 
 };
 
 #endif //NETWORK_HPP
