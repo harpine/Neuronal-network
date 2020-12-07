42ed64b952067b7ce5ee9950e82d1c778af700bb
Aline Brunner <aline.brunner@epfl.ch>
2020-12-07 14:57:03 +0100

    modifs rapport

--- src/network.cpp
+++ src/network.cpp
@@ -1,7 +1,7 @@
 #include "network.hpp"
-#include <algorithm>
 #include "inhibitoryNeuron.hpp"
 #include "excitatoryNeuron.hpp"
+#include <algorithm>
 
 Network::Network(char model, int nb, double p_E, double intensity, double lambda, double delta)
     : _intensity(intensity), _model(model)
